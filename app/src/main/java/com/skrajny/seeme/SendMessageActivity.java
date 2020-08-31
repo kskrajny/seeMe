5d2463f91fe644ea7bb307568e0cdcd0e5a8a5d1
kskrajny <j.skrajny@student.uw.edu.pl>
2020-08-31 18:06:09 +0200

    Program wysyła pakiet UDP na moj komputer. Postaram się zrobić multicast.

--- app/src/main/java/com/skrajny/seeme/SendMessageActivity.java
+++ app/src/main/java/com/skrajny/seeme/SendMessageActivity.java
@@ -10,28 +10,52 @@ import java.net.InetAddress;
 
 public class SendMessageActivity extends AppCompatActivity {
 
-    DatagramSocket socket;
-    InetAddress group;
-    byte[] buf;
+    private String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_message);
-        buf = getIntent().getExtras().getString("date").getBytes();
+        date = getIntent().getExtras().getString("date");
     }
 
-    public void multicast(View view) {
+    public void shareTime(View view) {
+        Thread thread = new sender(9090, "192.168.8.100", date);
+        thread.start();
         try {
-            socket = new DatagramSocket();
-            group = InetAddress.getByName("230.0.0.0");
-            DatagramPacket packet
-                    = new DatagramPacket(buf, buf.length, group, 4446);
-            socket.send(packet);
-        } catch (Exception e) {
+            thread.join();
+        } catch(Exception e) {
             e.printStackTrace();
         }
-        socket.close();
-        finish();
     }
+
+    private static class sender extends Thread {
+
+        private final int port;
+        private final String addr;
+        private final byte[] buff;
+        private byte[] receiveData;
+
+        public sender(int port, String addr, String mess) {
+            this.port = port;
+            this.addr = addr;
+            this.buff = mess.getBytes();
+            //receiveData = new byte[2];
+        }
+
+        public void run() {
+            try {
+                InetAddress inetAddress = InetAddress.getByName(addr);
+                DatagramSocket socket = new DatagramSocket(port);
+                DatagramPacket send_packet = new DatagramPacket(buff, buff.length, inetAddress, port);
+                socket.send(send_packet);
+                //DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                //socket.receive(receivePacket);
+                //Log.i("seeMe", new String(receivePacket.getData()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
